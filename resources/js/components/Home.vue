596c5e677b12297552bf41a6fcc91424c3fa47eb
Claudia Silvestre <cla.silvestremu@alum.uca.es>
2022-04-14 13:37:54 +0200

    Starting header

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <app-header />
+    </div>
+</template>
+
+<script>
+import header from './layouts/header.vue'
+
+export default {
+    name: 'Home',
+
+    components: {
+        'app-header': header
+    }
+}
+</script>
---
+++ resources/js/components/layouts/header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h3>{{ title }}</h3>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "What's New"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        background-color: black;
+        padding: 10px;
+        color: white;
+        margin: auto;
+    }
+</style>
