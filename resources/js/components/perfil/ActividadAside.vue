38468c2c7e102aeaecec44afcc6cfe3b6c151027
Claudia Silvestre <cla.silvestremu@alum.uca.es>
2022-09-11 20:04:04 +0200

    Fix

--- resources/js/components/perfil/ActividadAside.vue
+++ resources/js/components/perfil/ActividadAside.vue
@@ -5,7 +5,7 @@
             :key="'light'"
         ></b-spinner>
     </div>
-    <div v-else class="p-2 mt-4 background2 activity-aside">
+    <div v-else class="p-2 mt-4 background2 aside-activity">
         <h3 class="title-style titlePerfil">Actividad amigos</h3>
         <p v-if="actividadTotal.length === 0">Comienza a seguir a un amigo para ver su actividad.</p>
         <div v-else v-for="actividad in actividadTotal" :key="actividad.id" class="p-1 mt-2 mb-2 rounded">
